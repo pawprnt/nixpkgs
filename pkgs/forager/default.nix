774c53110ae381e06638527a598b72ab05d2db1f
foxinwinter <179904685+foxinwinter@users.noreply.github.com>
2026-08-31 05:42:48 -0500

    add forager and softie packages

---
+++ pkgs/forager/default.nix
@@ -0,0 +1,42 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, pyside6
+, evdev
+, keyring
+, pillow
+, qrcode
+}:
+
+buildPythonPackage {
+  pname = "forager";
+  version = "0.5.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "pawprnt";
+    repo = "forager";
+    rev = "v0.5.0";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  build-system = [ setuptools ];
+
+  propagatedBuildInputs = [
+    pyside6
+    evdev
+    keyring
+    pillow
+    qrcode
+  ];
+
+  pythonImportsCheck = [ "forager" ];
+
+  meta = with lib; {
+    description = "Steam-like game launcher for your local game library";
+    homepage = "https://github.com/pawprnt/forager";
+    license = licenses.agpl3Only;
+    mainProgram = "forager";
+  };
+}
